7b26b313d4a0a1930ef8822baf4aa96ec4914012
djnokin <nokindj@gmail.com>
2020-10-22 11:53:35 +0100

    done

--- src/movies.js
+++ src/movies.js
@@ -1,15 +1,88 @@
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors. How could you "clean" a bit this array and make it unified (without duplicates)?
-
+let getAllDirectors = function(movies) {
+    return movies.map((movie) => movie.director);
+}
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-
+let howManyMovies = (movies) => {
+        let spielbergDramaMovies = movies.filter(movie => {
+            return movie.genre.includes("Drama") && movie.director === "Steven Spielberg";
+        });
+        return spielbergDramaMovies.length; 
+}
 // Iteration 3: All rates average - Get the average of all rates with 2 decimals
-
+let ratesAverage = (movies) => {
+    if (movies.length === 0) {
+        return 0;
+    }
+    let totalSum = 0; 
+    movies.forEach((movie) => {
+        if (!movie.rate) {
+            movie.rate = 0;
+        }
+        totalSum += movie.rate;
+    });
+    let average = totalSum / movies.length;
+    return Number(average.toFixed(2)); // toFixed -> rounds up the number of decimal places in the parenteses, in this case (2).
+}                                       // toFixed converts the number to a string, so we have to use Number() to convert to a number
+// Other solution using reduce (more elegant)
+/*
+let ratesAverage = (movies) => {
+    if (movies.length === 0) {
+        return 0;
+    }
+    let reduceSum = movies.reduce((accumulator, currentValue) => {
+        if (!movie.rate) {
+            movie.rate = 0;
+        }
+        return accumulator + currentValue;
+    }, 0);
+    console.log("reduceSum", reduceSum);
+    let avg = reduceSum / movies.length;
+    return Number(avg.toFixed(2));
+}
+*/
 // Iteration 4: Drama movies - Get the average of Drama Movies
+let dramaMoviesRate = ((movies) => {
+    let dramaMovies = movies.filter (movie => movie.genre.includes("Drama"));
+    return ratesAverage (dramaMovies);
+});
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-
+let orderByYear = (movies) => {
+    let orderedMovies = movies.sort((movie1, movie2) => {
+        if (movie1.year === movie2.year) {
+            if (movie1.title > movie2.title) {
+                return 1;
+            } else {
+                return -1;
+            }
+        } else {
+            if (movie1.year > movie2.year) {
+                return 1;
+            } else {
+                return -1;
+            }
+        }
+    });
+    // spread operator [...] creates a new Array like .map.
+    return [...orderedMovies];
+}
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
+const orderAlphabetically = (movies) => {
+    let orderedArray = [...movies].sort((movie1, movie2) => { //use the spread operator on movies to create a new array but don't mutate them. 
+        if (movie1.title > movie2.title) {
+            return 1;
+        } else {
+            return -1;
+        }
+    }).slice(0, 20); // to slice from index 0 to 20
+    return orderedArray.map(movie => movie.title);
+}
+// Sort for Arrays.
+// if you are ordering strings -> .sort()
+// if you are ordering number -> .sort( <function inside> )
+// if you are ordering number -> .sort( <function inside> )
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 
